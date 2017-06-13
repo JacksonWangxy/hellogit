012e3fe63f2c1c4ba3005337911984c04ff5a67c
王新宇 <wangxinyu@192.168.8.101>
2017-06-14 00:09:23 +0800

    version1

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * PACKAGE_NAME
+ *
+ * @author wangxinyu
+ * @create 2017-06-14 00:01
+ **/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
